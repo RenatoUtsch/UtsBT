ad86040596e82e36b990b4353428698b27d2ff01
Renato Utsch <renatoutsch@gmail.com>
2011-07-18 22:20:51 -0300

    Additions:   * Added the COPYING (the license) file to the project.
    Bugfixes:
      * Modified some things to make the GNU GPLv3 license ok.

--- UtsBT.h
+++ UtsBT.h
@@ -8,13 +8,18 @@
  *
  * Copyright (C) 2011, Renato Utsch, <renatoutsch@gmail.com>
  *
- * This software is licensed under GNU GPL license, as described
- * in the file COPYING, which you should have receibed as part of
- * this distribution. The terms are also available at:
- * http://www.gnu.org/licenses/gpl.html
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * This software doesn't have ANY WARRANTY OF ANY KIND, either
- * express or implied.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  **************************************************************************************************************
  *
